fb07cae4d9c20573e05eb973c38bc8273e1d44c7
sahrulprograming <sahrulsaefulah@gmail.com>
2022-10-22 18:29:18 +0700

    menambahkan button login di beranda

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -110,6 +110,13 @@
                                 <a href="#"
                                     class="block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-[#F58634] md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">kontak</a>
                             </li>
+                            <li>
+                                <a href="/login"
+                                    class="outline outline-[#F58634] text-[#F58634] capitalize bg-white font-[Poppins] font-bold text-xl duration-500 px-8 py-1 mx-4 hover:bg-[#F58634] hover:text-white hover:outline-white rounded cursor-pointer"
+                                    style="border-radius: 16px 32px 0px 16px">
+                                    masuk / daftar
+                                </a>
+                            </li>
                         </ul>
                     </div>
                 </div>
